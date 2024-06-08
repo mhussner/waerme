ac1505cdc618ed31bbac477ffdffb0f3a8a79988
Lastmanagment KE (extern) <einkee>
2024-06-08 20:29:55 +0200

    Kommentare in .bat datei

--- run_waerme.bat
+++ run_waerme.bat
@@ -1,5 +1,10 @@
+Rem Starte Anaconda prompt (hier den Pfad zur .bat einfügen, der die anaconda prompt öffnet)
 call C:\Users\einkee\AppData\Local\miniconda3\Scripts\activate.bat
+Rem activiere virtual environment mit nötigen python libraries
 call conda activate warm
+Rem change directory zu Ordner mit der main Python Datei waerme.py
 call cd C:\Users\einkee\Documents\waerme
+Rem benutzr streamlit run command um das web gui zu öffnen und befehle aus python Datei auszuführen
 call streamlit run waerme.py
+Rem cmd \k verhindert, dass sich Anaconda prompt und web gui von selbst schließen
 cmd \k
